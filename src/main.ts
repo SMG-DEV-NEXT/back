147d0e41184d1a61d613c542d5b2f4f6d63e6135
SMG-DEV-NEXT <smgdevelopmentnest@gmail.com>
2025-04-20 22:16:28 +0400

    changes

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import helmet from "helmet";
+import helmet from 'helmet';
 import rateLimit from 'express-rate-limit';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser())
+  app.use(cookieParser());
   app.use(
     rateLimit({
       windowMs: 15 * 60 * 1000, // 15 minutes
       max: 100, // Limit each IP to 100 requests per window
-    })
+    }),
   );
-  
+
   app.useGlobalPipes(new ValidationPipe());
-  app.use(helmet({
-    referrerPolicy:{policy:"origin-when-cross-origin"}
-  })); // Adds security headers
+  app.use(
+    helmet({
+      referrerPolicy: { policy: 'origin-when-cross-origin' },
+    }),
+  ); // Adds security headers
   app.enableCors({
-    origin: "http://localhost:3000", // Replace with frontend URL
+    origin: process.env.FRONT_URL, // Replace with frontend URL
     credentials: true, // Allow cookies
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
   });
   await app.listen(process.env.PORT ?? 4000);
 }
 
-
-bootstrap()
+bootstrap();
