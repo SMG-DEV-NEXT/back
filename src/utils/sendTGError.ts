b65c495d2268cb3b8a5284e1cd4e79cf5a77b082
SMG-DEV-NEXT <smgdevelopmentnest@gmail.com>
2025-05-09 14:39:13 +0400

    changes

--- src/utils/sendTGError.ts
+++ src/utils/sendTGError.ts
@@ -4,12 +4,18 @@ async function sendErrorNotification(error: any) {
   const message = `❗️ Server crashed\n\nError: ${error.message || error}`;
   const token = '8142669648:AAGZ1kRQuE14ICPPzROTiCByyaXPwMBlzQQ';
   const chatId = '1128697965';
-  console.log(error);
+  const allowedChatIds = ['1128697965', '1083448745', '1482996433'];
   try {
-    await axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
-      chat_id: chatId,
-      text: message,
-    });
+    for (const chatId of allowedChatIds) {
+      try {
+        await axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
+          chat_id: chatId.trim(),
+          text: message,
+        });
+      } catch (e) {
+        console.error(`❌ Failed to send Telegram message to ${chatId}:`, e);
+      }
+    }
   } catch (e) {
     console.error('Failed to send Telegram message:', e);
   }
