c3e7a24a0efc67d0b29bedb01e1704e08f822f2f
SMG-DEV-NEXT <smgdevelopmentnest@gmail.com>
2025-05-27 22:55:46 +0400

    erro

--- src/checkout/checkout.module.ts
+++ src/checkout/checkout.module.ts
@@ -4,9 +4,11 @@ import { CheckoutController } from './checkout.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { SmtpService } from 'src/smtp/smtp.service';
 import { CryptoService } from 'src/crypto/crypto.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   controllers: [CheckoutController],
   providers: [CheckoutService, PrismaService, SmtpService, CryptoService],
+  imports: [HttpModule],
 })
 export class CheckoutModule {}
