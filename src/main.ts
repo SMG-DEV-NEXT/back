096b9e6ec4c6d08357bdb69176a28f2e2daeccfe
SMG-DEV-NEXT <smgdevelopmentnest@gmail.com>
2025-07-13 12:00:54 +0400

    changes

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
   app.use(urlencoded({ limit: '1024mb', extended: true }));
   app.use(json({ limit: '1024mb' }));
   const uploadPath = process.env.UPLOAD_PATH || 'uploads';
-  console.log('aaaa');
+  console.log('aaaaa');
   app.useStaticAssets(join(process.cwd(), '..', uploadPath), {
     prefix: '/api/uploads/',
     setHeaders: (res, path) => {
