9d8c61b0aedff398acdb05877fddff5d9ac6359e
SMG-DEV-NEXT <smgdevelopmentnest@gmail.com>
2025-10-30 00:00:40 +0400

    changes

--- src/checkout/checkout.service.ts
+++ src/checkout/checkout.service.ts
@@ -159,13 +159,12 @@ export class CheckoutService {
           orderId,
         },
       });
-      const amountStr = Number(finalPrice).toFixed(2); // "2000.00"
+      // const amountStr = Number(finalPrice).toFixed(2); // "2000.00"
       const signature = crypto
         .createHash('md5')
-        .update(`${this.merchantId}:${amountStr}:${this.secret1}:${orderId}`)
+        .update(`${this.merchantId}:${finalPrice}:${this.secret1}:${orderId}`)
         .digest('hex');
-      const payUrl = `https://pay.fk.money/?m=${this.merchantId}&oa=${amountStr}&o=${orderId}&s=${signature}&currency=RUB`;
-
+      const payUrl = `https://pay.fk.money/?m=${this.merchantId}&oa=${finalPrice}&i=&currency=RUB&em=&phone=&o=${orderId}&pay=PAY&s=e723c585cb601241c5bb5727efa16b08`;
       // await this.handleCallback({
       //   status: 'succeeded',
       //   external_id: transaction.id,
